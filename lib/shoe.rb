4dff3ea938549d162822ea7d2316f25ffe322fb5
Nsingletary <singletarybush@gmail.com>
2020-12-20 02:18:18 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,6 @@
 attr_accessor :color, :size, :material, :condition
 attr_reader :brand
 
-BRANDS = []
-
 def initialize(brand)
   @brand = brand
   if BRANDS.include?(brand) == false
@@ -17,4 +15,3 @@ def cobble
 end
 
 end 
-end
